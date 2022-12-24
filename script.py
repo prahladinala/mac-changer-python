d506b657345462cd6360697d1c6b0294bd7ed599
Prahlad <prahladjngp@gmail.com>
2022-12-24 21:56:04 +0530

    Added Function for MAC Changer

--- script.py
+++ script.py
@@ -1,6 +1,14 @@
 import subprocess
 import optparse
 
+# Function for MAC Changer
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+    # Mac Changer Script (Additional Security)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
 # Options - Optparse
 parser = optparse.OptionParser()
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
@@ -8,19 +16,9 @@ parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 (options, arguments) = parser.parse_args()
 
 # Variables
-interface = options.interface
-new_mac = options.new_mac
+# interface = options.interface
+# new_mac = options.new_mac
 ## NOTE: input() or raw_input() is used to get user input
 
-# Output Text
-print("[+] Changing MAC Address for " + interface + " to " + new_mac)
-
-# Mac Changer Script
-# subprocess.call("ifconfig " + interface + " down", shell=True)
-# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-# subprocess.call("ifconfig " + interface + " up", shell=True)
-
-# Mac Changer Script (Additional Security)
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+# Calling Mac Changer Function
+change_mac(options.interface, options.new_mac)
